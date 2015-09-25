027a306e620e59aaaa3b85c66960c73c64351d32
Erik Goodman <erkorivo@catworks.arizona.edu>
2015-09-25 11:07:01 -0700

    solved json unicode encoding by using ruamel.yaml instead

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from bs4 import BeautifulSoup
+from pprint import pprint
 import json
 import os
-from pprint import pprint
-import selenium.webdriver as webdriver
-# import simplejson as json
 import requests
-import urllib2
+import ruamel.yaml as yaml
 
 # access_token = # add IG access token
 client_id = os.environ['tIG_CLIENT_ID'] # IG client id (env var)
 INSTAGRAM_API = 'https://api.instagram.com/v1/'
-# call_IG(tag="tucson")
 
-def call_IG(tag="nofilter"):
+def build_url(tag="nofilter"):
     # ugly string concat for 'recent' results of provided tag.
-    raw_url = INSTAGRAM_API + 'tags/' + tag + "/media/recent?client_id=" + client_id
-    soup = BeautifulSoup(urllib2.urlopen(raw_url).read(), "html.parser")
+    return INSTAGRAM_API + 'tags/' + tag + "/media/recent?client_id=" + client_id
 
-    results = soup.findAll("link")
-    pprint(results)
-    # dict=json.loads(str(soup))
-    # pprint(dict)
-    # links = dict["u'link'"]
-    # pprint(links)
-    # for x in soup.findAll('link'):
-    #     print x
-
-call_IG(tag="tucson")
+def get_page(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    # pprint(soup)
+    # j = json.loads(str(soup))
+    # pprint(j)
+    y = yaml.load(str(soup))
+    pprint(y)
+raw_url = build_url(tag="tucson")
+page = get_page(raw_url)
