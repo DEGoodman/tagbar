ee0e741ddee875699da807eb986ee2d1cb20df00
Erik Goodman <erkorivo@catworks.arizona.edu>
2015-09-25 12:59:51 -0700

    isolate lniks from tags

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from bs4 import BeautifulSoup
 from pprint import pprint
-import json
 import os
 import requests
-import ruamel.yaml as yaml
 
 # access_token = # add IG access token
 client_id = os.environ['tIG_CLIENT_ID'] # IG client id (env var)
@@ -13,15 +10,11 @@ def build_url(tag="nofilter"):
     # ugly string concat for 'recent' results of provided tag.
     return INSTAGRAM_API + 'tags/' + tag + "/media/recent?client_id=" + client_id
 
-def get_page(url):
+def get_links(url):
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    # pprint(soup)
-    # j = json.loads(str(soup))
-    # pprint(j)
-    y = yaml.load(str(soup))
-    pprint(y)
-    pprint(y['link'])
+    j = page.json()
+    return [obj["link"] for obj in j["data"]]
 
 raw_url = build_url(tag="tucson")
-page = get_page(raw_url)
+links = get_links(raw_url)
+print(links)
