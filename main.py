f598a59b4265bdb0f7bbb5a258b1a6275bd81bd3
Erik <goodmanderik@gmail.com>
2015-10-14 14:38:21 -0700

    make everything waaay easier by using IG-python module

--- main.py
+++ main.py
@@ -1,44 +1,20 @@
+from instagram.client import InstagramAPI
 from pprint import pprint
 import os
 import requests
 import sys
 import urllib3
-# access_token = # add IG access token
+
 client_id = os.environ['tIG_CLIENT_ID'] # IG client id (env var)
-INSTAGRAM_API = 'https://api.instagram.com/v1/'
+client_secret = os.environ['tIG_CLIENT_SECRET'] # IG client secret (env var)
 
-def build_url(tag="nofilter"):
-    # ugly string concat for 'recent' results of provided tag.
-    return INSTAGRAM_API + 'tags/' + tag + "/media/recent?client_id=" + client_id
+api = InstagramAPI(client_id=client_id, client_secret=client_secret)
 
-def get_img(url):
-    r = requests.get(url)
-    # this is hopefully a 200
-    print(r.status_code)
-    # Let's see what we get back!
-    print(r.text)
+tag_name=sys.argv[1]
+pprint("looking for \'%s\'." % tag_name)
+# return last 20 photos of tag
+recent = api.tag_recent_media(30, 1000000000, tag_name)
 
-
-def get_links(url):
-    j = requests.get(url).json() # jsonify tag API query
-    return [obj["link"] for obj in j["data"]] # get all links from ^
-
-# for flexibility
-def get_storage(static="static/"):
-    return static
-
-#enter a tab when calling the program
-raw_url = build_url(tag=sys.argv[1])
-print("looking for \'%s\'." % sys.argv[1])
-links = get_links(raw_url)
-print(links)
-path = get_storage()
-
-# let's get our photos
-# onlu one for now to save on request #'s
-img = get_img(links[0])
-# TODO: 
-# for p in links:
-#     img = get_img(p)
-
-
+# recent[0] is list of returned media
+for media in recent[0]:
+    pprint(media.images['standard_resolution'].url)
