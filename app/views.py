638bd6ab92ea64b8f18b70710808a131bf4f78e7
Erik <goodmanderik@gmail.com>
2015-11-08 20:54:50 -0700

    import common colors to results page

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app import app
 from .forms import TagForm
 from .get_ig_photos import Setup
 from .analyzer import Analyze
+import process
 
 @app.route('/')
 @app.route('/index')
@@ -20,7 +21,8 @@ def search():
         # the below line will import new images by tag. Commenting out for dev
         # Setup(form.tag.data)
         Analyze()
-        return redirect(url_for('results',tag=form.tag.data))
+        cols = [process.compile()]
+        return redirect(url_for('results',tag=form.tag.data, main_cols=cols))
     return render_template('query.html',
                            title='Tag Search',
                            form=form)
@@ -30,4 +32,5 @@ def results():
     state = 'dev'
     return render_template('results.html',
                            title='tagbar',
-                           hashtag=request.args.get('tag'))
+                           hashtag=request.args.get('tag'),
+                           colors=request.args.get('main_cols'))
