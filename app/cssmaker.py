0201df90896ced8d731761fba5565cd1dd71d789
Erik <goodmanderik@gmail.com>
2015-11-12 23:18:04 -0700

    write and load base colors

---
+++ app/cssmaker.py
@@ -0,0 +1,27 @@
+import os
+
+from pprint import pprint
+
+def make(data):
+    pprint(data)
+    f = open(os.getcwd() + '/app/static/colors.css', 'w')
+    pos_base = ['a', 'b', 'c', 'd', 'e']
+    num = 0
+    # not sure why data is a nested list
+    for quant,col  in data[0]:
+        print(quant)
+        print(col)
+        f.write('#%s' % pos_base[num])
+        f.write(' {\n\tbackground-color: rgb') # nastiness
+        f.write(str(col))
+        f.write(';\n}\n\n')
+        num += 1
+
+    pos_comp = ['f', 'g', 'h', 'i', 'j']
+    num = 0
+    newcols = comp_cols(data)
+
+    f.close()
+
+def comp_cols(data):
+    pass
