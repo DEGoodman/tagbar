13b6ecab81ea38f0923d1fa4141de52066857535
Erik <goodman.d.erik@gmail.com>
2015-11-23 22:49:18 -0700

    summing to create average image is much faster than sorting dict of pixels of many imgs

--- app/analyzer.py
+++ app/analyzer.py
@@ -24,37 +24,44 @@ class Analyze():
         imlist=self.imlist
         # Assuming all images are the same size, get dimensions of first image
         w,h=Image.open(imlist[0]).size
-        # N=len(imlist)
-        #
-        # # Create a numpy array of floats to store the average (assume RGB images)
-        # arr=numpy.zeros((h,w,3),numpy.float)
-        #
-        # # Build up average pixel intensities, casting each image as an array of floats
-        # for im in imlist:
-        #     imarr=numpy.array(Image.open(im),dtype=numpy.float)
-        #     # arr=arr+imarr/N
-        #     arr += imarr
-        #
-        # # Round values in array and cast as 8-bit integer
-        # arr=numpy.array(numpy.round(arr),dtype=numpy.uint8)
-        #
-        # # Generate, save and preview final image
-        # out=Image.fromarray(arr,mode="RGB")
-        # out.save(self.img_dir + "Average.jpg")
-        # out.show()
+        N=len(imlist)
+
+        # Create a numpy array of floats to store the average (assume RGB images)
+        arr=numpy.zeros((h,w,3),numpy.float)
+
+        # Build up average pixel intensities, casting each image as an array of floats
+        for im in imlist:
+            imarr=numpy.array(Image.open(im),dtype=numpy.float)
+            # arr=arr+imarr/N
+            arr += imarr
+
+        # Round values in array and cast as 8-bit integer
+        arr=numpy.array(numpy.round(arr),dtype=numpy.uint8)
+
+        # Generate, save and preview final image
+        out=Image.fromarray(arr,mode="RGB")
+        out.save(self.img_dir + "Average.jpg")
+        out.show()
 
         # new combine method
-        all_pixels = []
-        for im in imlist:
-            i = Image.open(im)
-            pixels=i.load()
-            for x in range(w):
-                for y in range(h):
-                    cpixel = pixels[x, y]
-                    all_pixels.append(cpixel)
+        # all_pixels = {}
+        # for im in imlist:
+        #     i = Image.open(im)
+        #     pixels=i.load()
+        #     for x in range(w):
+        #         for y in range(h):
+        #             cpixel = pixels[x, y]
+        #             all_pixels.setdefault(cpixel, 0)
+        #             all_pixels[cpixel] += 1
+        #             # all_pixels[cpixel] = all_pixels.get(cpixel, 0) + 1
+        #
+        #
+        # print("got pixels. collating data")
+        # counts = [(j,i) for i,j in all_pixels.items()]
+        # count, max_elm = max(counts)
+        # pprint(sorted(counts))
 
-        print("got pixels. collating data")
-        result = dict()
-        for tup in set(all_pixels):
-            result[tup] = all_pixels.count(tup)
-        pprint(result)
+        # result = dict()
+        # for tup in set(all_pixels):
+        #     result[tup] = all_pixels.count(tup)
+        # pprint(result)
--- app/get_ig_photos.py
+++ app/get_ig_photos.py
@@ -24,7 +24,7 @@ class Setup:
         pprint("looking for \'%s\'..." % self.tag_name)
         # return last photo of tag
         # this is because the colors turn out better if only one image is used
-        self.recent = self.api.tag_recent_media(4, 1000000000, self.tag_name)
+        self.recent = self.api.tag_recent_media(2, 1000000000, self.tag_name)
         # recent[0] is list of returned media
         for media in self.recent[0]:
             self.keep(media.images['standard_resolution'].url)
