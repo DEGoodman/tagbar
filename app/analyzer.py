87d8f2f8fcf3c2a7fd0e1c3ddb9ec72832ef415b
Erik <goodman.d.erik@gmail.com>
2015-12-03 05:24:02 -0700

    add select one of 10 images

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,5 +1,6 @@
 import os,sys
 import numpy
+import random
 
 from collections import Counter
 from PIL import Image
@@ -21,22 +22,19 @@ class Analyze():
 
     # see: https://stackoverflow.com/questions/17291455/how-to-get-an-average-picture-from-100-pictures-using-pil
     def combine(self):
-        imlist=self.imlist
+        im = random.choice(self.imlist)
         # Assuming all images are the same size, get dimensions of first image
-        w,h=Image.open(imlist[0]).size
-        N=len(imlist)
+        w,h=Image.open(im).size
+        N=len(im)
 
         # Create a numpy array of floats to store the average (assume RGB images)
         arr=numpy.zeros((h,w,3),numpy.float)
 
         # Build up average pixel intensities, casting each image as an array of floats
-        for im in imlist:
-            imarr=numpy.array(Image.open(im),dtype=numpy.float)
-            # arr=arr+imarr/N
-            arr += imarr
+        imarr=numpy.array(Image.open(im),dtype=numpy.float)
 
         # Round values in array and cast as 8-bit integer
-        arr=numpy.array(numpy.round(arr),dtype=numpy.uint8)
+        arr=numpy.array(numpy.round(imarr),dtype=numpy.uint8)
 
         # Generate, save and preview final image
         out=Image.fromarray(arr,mode="RGB")
