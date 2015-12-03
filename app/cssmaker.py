50b636dccf812d74404094ccd012df78ce989c0d
Erik <goodman.d.erik@gmail.com>
2015-12-03 04:26:41 -0700

    better boxes

--- app/cssmaker.py
+++ app/cssmaker.py
@@ -8,8 +8,8 @@ from pprint import pprint
 
 def make(data):
     f = open(os.getcwd() + '/app/static/css/colors.css', 'w')
-    f.write('.color-swatches {\n\tmargin: 5px 0px -5px 0px;\n\twidth:550px;\n\toverflow:hidden;\n}\n')
-    f.write('.color-swatch {\n\tfloat: left;\n\tmargin: 0px 5px;\n\tborder-radius: 3px;\n\twidth: 100px;\n\theight: 100px;\n}\n')
+    # f.write('.color-swatches {\n\tmargin: 5px 0px -5px 0px;\n\twidth:550px;\n\toverflow:hidden;\n}\n')
+    # f.write('.color-swatch {\n\tfloat: left;\n\tmargin: 0px 5px;\n\tborder-radius: 3px;\n\twidth: 100px;\n\theight: 100px;\n}\n')
 
     pos_base = ['a', 'b', 'c', 'd', 'e']
     num = 0
